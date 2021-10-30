9e5a8a51e75a54780ebee45113593180f7c8ddbe
m-fidow <muna.a.fidow@gmail.com>
2021-10-30 21:31:58 +0100

    can sign up and user will be added to users database

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-const App = () => {
+import { signup } from './firebase';
+import { useRef } from 'react';
+function App() {
+	const emailRef = useRef();
+	const passwordRef = useRef();
+	async function handleSignup() {
+		await signup(emailRef.current.value, passwordRef.current.value);
+	}
 	return (
-		<div className="main">
-			<input placeholder="email" type="email" />
-			<input placeholder="Password" type="password" />
+		<div id="main">
+			<div id="fields">
+				<input ref={emailRef} placeholder="email" />
+				<input ref={passwordRef} placeholder="Password" type="password" />
+			</div>
+			<button onClick={handleSignup}>SignUP</button>
 		</div>
 	);
-};
+}
 
 export default App;
