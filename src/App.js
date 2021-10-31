833a5e4af1429ee07150b37d316845188c8c0685
m-fidow <muna.a.fidow@gmail.com>
2021-10-31 15:44:33 +0000

    form

--- src/App.js
+++ src/App.js
@@ -4,35 +4,38 @@ import Payment from './components/Payment';
 import Homepage from './components/Homepage';
 import Signup from './components/Signup';
 import Box from '@mui/material/Box';
+
 import Results from './components/Results';
 
 export default function App() {
 	return (
-		<Payment />
+		<>
+			<Router>
+				<Switch>
+					<Route exact path="/">
+						<Box
+							sx={{
+								display: 'flex',
+								justifyContent: 'space-around'
+							}}
+						>
+							<Box sx={{ alignSelf: 'left' }}>
+								<Signup />
+							</Box>
+							<div>
+								<Homepage />
+							</div>
+						</Box>
+					</Route>
+					<Route path="/payment">
+						<Payment />
+					</Route>
 
-		// <>
-		// 	<Router>
-		// 		<Switch>
-		// 			<Route exact path="/">
-		// 				<Box
-		// 					sx={{
-		// 						display: 'flex',
-		// 						justifyContent: 'space-around'
-		// 					}}
-		// 				>
-		// 					<Box sx={{ alignSelf: 'left' }}>
-		// 						<Signup />
-		// 					</Box>
-		// 					<div>
-		// 						<Homepage />
-		// 					</div>
-		// 				</Box>
-		// 			</Route>
-		// 			<Route path="/carbon">
-		// 				<Carbon />
-		// 			</Route>
-		// 		</Switch>
-		// 	</Router>
-		// </>
+					<Route path="/carbon">
+						<Carbon />
+					</Route>
+				</Switch>
+			</Router>
+		</>
 	);
 }
--- src/components/Payment.js
+++ src/components/Payment.js
@@ -1,7 +1,39 @@
 import React from 'react';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import Logo from './Logo';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import Paper from '@mui/material/Paper';
+import PaymentForm from './PaymentForm';
+import { Grid } from '@mui/material';
+import car from './../assets/undraw_electric_car_b-7-hl.svg';
 
 function Payment() {
-	return <div>HELLo</div>;
+	return (
+		<Container component="main" maxWidth="lg">
+			<Box>
+				<Logo />
+				<Box sx={{ display: 'flex', width: '100%', gap: '30px' }}>
+					<Box sx={{ flex: '1' }}>
+						<Box
+							component="img"
+							sx={{
+								maxWidth: 650,
+								paddingTop: 5
+							}}
+							alt="car"
+							src={car}
+						/>
+						{/* <img src={car} alt="car" sx={{ maxWidth: '100%' }} /> */}
+					</Box>
+					<PaymentForm />
+				</Box>
+			</Box>
+		</Container>
+	);
 }
 
 export default Payment;
