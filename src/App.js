922393512ce72237ce60a691282816bf7836e374
m-fidow <muna.a.fidow@gmail.com>
2021-10-31 13:56:31 +0000

    initialised

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import Carbon from './components/Carbon';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-
+import Payment from './components/Payment';
 import Homepage from './components/Homepage';
 import Signup from './components/Signup';
 import Box from '@mui/material/Box';
@@ -8,33 +8,31 @@ import Results from './components/Results';
 
 export default function App() {
 	return (
+		<Payment />
 
-
-
-		<>
-			<Router>
-				<Switch>
-					<Route exact path="/">
-						<Box
-							sx={{
-								display: 'flex',
-								justifyContent: 'space-around'
-							}}
-						>
-							<Box sx={{ alignSelf: 'left' }}>
-								<Signup />
-							</Box>
-							<div>
-								<Homepage />
-							</div>
-						</Box>
-					</Route>
-					<Route path="/carbon">
-						<Carbon />
-					</Route>
-				</Switch>
-			</Router>
-		</>
-
+		// <>
+		// 	<Router>
+		// 		<Switch>
+		// 			<Route exact path="/">
+		// 				<Box
+		// 					sx={{
+		// 						display: 'flex',
+		// 						justifyContent: 'space-around'
+		// 					}}
+		// 				>
+		// 					<Box sx={{ alignSelf: 'left' }}>
+		// 						<Signup />
+		// 					</Box>
+		// 					<div>
+		// 						<Homepage />
+		// 					</div>
+		// 				</Box>
+		// 			</Route>
+		// 			<Route path="/carbon">
+		// 				<Carbon />
+		// 			</Route>
+		// 		</Switch>
+		// 	</Router>
+		// </>
 	);
 }
---
+++ src/components/Payment.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Payment() {
+	return <div>HELLo</div>;
+}
+
+export default Payment;
