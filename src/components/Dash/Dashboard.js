f1562601254e321898031e83127ca1715b095e73
m-fidow <muna.a.fidow@gmail.com>
2021-10-31 18:04:21 +0000

    absolutute positioned worl

--- src/components/Dash/Dashboard.js
+++ src/components/Dash/Dashboard.js
@@ -13,6 +13,7 @@ const Dashboard = () => {
 			<Container component="main" maxWidth="lg">
 				<Logo />
 				<Box
+					mt={3}
 					sx={{
 						display: 'flex',
 						width: '100%',
@@ -22,24 +23,33 @@ const Dashboard = () => {
 					}}
 				>
 					<Box
-						component="img"
+						component="div"
 						sx={{
+							position: 'relative',
 							width: '95%',
 							height: '308px',
 							opacity: 0.5,
-							borderRadius: '12px'
+							borderRadius: '12px',
+							backgroundImage: `url(${dashheader})`,
+							backgroundSize: 'cover',
+							backgroundPosition: 'center'
 						}}
-						alt="dashboard-header"
-						src={dashheader}
+
+						// alt="dashboard-header",
+
+						// src={dashheader}
 					></Box>
 					<Box
 						sx={{
+							position: 'absolute',
 							display: 'flex',
-							width: '80%',
 							height: '114px',
 							background: 'rgba(174, 180, 183, 0.48)',
 							alignItems: 'center',
-							borderRadius: '12px'
+							borderRadius: '12px',
+							minWidth: '1017px',
+							top: '295px',
+							maxWidth: '100%'
 						}}
 					>
 						<Box
@@ -59,33 +69,26 @@ const Dashboard = () => {
 					</Box>
 					<Box
 						sx={{
+							width: '100%',
 							display: 'flex',
-							justifyContent: 'space-evenly'
+							justifyContent: 'space-evenly',
+							position: 'relative'
 						}}
 					>
 						{/* <Chart /> */}
-            <Box
+						<Box
 							component="img"
 							sx={{
-								width: '450px',
-								height: '450px',
-								borderRadius: '12px'
+								width: '30%',
+								// height: '450px',
+								borderRadius: '12px',
+								position: 'absolute',
+								right: '0',
+								top: '186px'
 							}}
 							alt="person-holding-globe"
 							src={world}
-						>
-            </Box>
-            <Box
-							component="img"
-							sx={{
-								width: '450px',
-								height: '450px',
-								borderRadius: '12px'
-							}}
-							alt="person-holding-globe"
-							src={world}
-						>
-            </Box>
+						></Box>
 					</Box>
 				</Box>
 			</Container>
